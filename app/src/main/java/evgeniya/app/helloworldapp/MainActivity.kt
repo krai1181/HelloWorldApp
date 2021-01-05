8ca3072737cd44c47c75a145e2b57ec80b544882
evgeniyakrayzman <evgeniasimakov@gmaikl>
2021-01-05 16:17:09 +0200

    new test project

---
+++ app/src/main/java/evgeniya/app/helloworldapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package evgeniya.app.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
